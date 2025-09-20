283d8133cce1f642829316b911977e1ae5bc3f37
bot910 <bot910-official@outlook.com>
2025-09-20 15:26:11 +0200

    Updated file

--- utils.py
+++ utils.py
@@ -1,3 +1,25 @@
-print("test")
-print("test2")
-print("test3")
+import os
+import time
+import random
+
+def wait(amount):
+    time.sleep(amount)
+
+def current_time_millis():
+    return int(round(time.time() * 1000))
+
+def random_int(min_val, max_val):
+    return random.randint(min_val, max_val)
+
+def random_float(min_val, max_val):
+    return random.uniform(min_val, max_val)
+
+def random_choice(choices):
+    return random.choice(choices)
+
+def random_string(length):
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    return ''.join(random.choice(letters) for i in range(length))
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
